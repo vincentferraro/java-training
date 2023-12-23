b97bac702ff78f738fd5988c28456b7871b03361
vincentferraro <v.ferraro@live.fr>
2023-12-23 11:18:55 -0500

    add class server and data

---
+++ Data.java
@@ -0,0 +1,24 @@
+
+public class Data {
+    private String route;
+    private String data;
+
+    public Data(String r, String d){
+        this.route = r;
+        this.data = d;
+    }
+
+    public void setRoute(String route){
+        this.route = route;
+    }
+
+    public String getRoute(){
+        return this.route;
+    }
+    public void setData(String data){
+        this.data = data;
+    }
+    public String getData(){
+        return this.data;
+    }
+}
---
+++ server.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+
+public class server {
+    
+    private ArrayList<String> routes;
+    private ArrayList<Data> datas;
+
+    public boolean setRoute(String routeName){
+
+        if(this.routes == null){
+            return false;
+        }else{
+            if(!this.routes.contains(routeName)){
+            this.routes.add(routeName);
+            return true;
+        }else{
+            return false;
+        }
+        }
+        
+    }
+
+    public boolean getRoute(String route){
+        if(this.routes.contains(route)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public boolean setData(Data data){
+        this.datas.add(data);
+        return true;
+    }
+
+    public Data getData(String routeName){
+        for (Data object : datas) {
+            if(object.getRoute() == routeName){
+                return object;
+            }
+        }
+        return null;
+
+    }
+
+    public static void main(String [] args){
+    server serv = new server();
+    // serv.setRoute("hello");
+    Data d1 = new Data("hello", "Hello World");
+    serv.setData(d1);
+
+    Data value = serv.getData("hello");
+    System.out.println(value);
+}
+}
+
+
