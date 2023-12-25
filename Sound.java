1f574e9040b07f99484ffa84ccb72e18a906a479
vincentferraro <v.ferraro@live.fr>
2023-12-25 14:11:16 -0500

    add exercice java

---
+++ Sound.java
@@ -0,0 +1,11 @@
+public class Sound {
+    
+    public static void main (String [] args){
+        double dist;
+
+        dist = 7.2 * 1100;
+
+        System.out.println("The lightening is "+dist+ " feet away.");
+
+    }
+}
