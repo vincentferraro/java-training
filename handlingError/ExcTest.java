fef63f7c02edc91d8f94dfc9febc0001902e4e66
vincentferraro <v.ferraro@live.fr>
2024-01-09 07:50:27 -0500

    Throwing exception

--- handlingError/ExcTest.java
+++ handlingError/ExcTest.java
@@ -1,5 +1,7 @@
 package handlingError;
 
+import java.lang.reflect.Array;
+
 public class ExcTest {
     static void genException(){
         int nums[] = new int[4];
@@ -22,3 +24,66 @@ class ExcDemo2{
         System.out.println("After catch statement");
     }
 }
+class excDemo3{
+    public static void main(String[] args) {
+        int number[] = { 4,8,16,32,64,128,256,212};
+        int denom [] = { 2,0,4,4,0,8};
+
+        for(int i = 0; i<number.length; i++){
+            try{
+                System.out.println(number[i] + " / " + denom[i]+ " is "+number[i]/denom[i]);
+            }catch( ArithmeticException exc){
+                System.out.println("Can't divide by Zero !");
+
+            }catch( ArrayIndexOutOfBoundsException exc){
+                System.out.println("No matching element found.");
+            }
+        }
+    }
+}
+
+class excDemo4{
+    public static void main(String[] args) {
+        int number[] = { 4,8,16,32,64,128,256,212};
+        int denom [] = { 2,0,4,4,0,8};
+
+        for(int i = 0; i<number.length; i++){
+            try{
+                System.out.println(number[i] + " / " + denom[i]+ " is "+number[i]/denom[i]);
+            }catch( ArrayIndexOutOfBoundsException exc){
+                System.out.println("No matching element found.");
+            }catch( Throwable exc){
+                System.out.println("Some exception occured");
+            }
+        }
+    }
+}
+
+class ThrowDemo{
+    public static void main(String[] args) {
+        try{
+            System.out.println("Before exception");
+            throw new ArithmeticException();
+        }catch(ArithmeticException exc){
+            System.out.println("Arimthmetic Exception occured");
+        }
+    }
+}
+
+class RethrowDemo{
+    public static void main(String[] args) {
+        try{
+            try{
+            System.out.println("Before exception");
+            throw new ArithmeticException();
+        }catch(ArithmeticException exc){
+            System.out.println("Arimthmetic Exception occured");
+            throw new Throwable();
+        }
+        }catch(Throwable exc){
+            System.out.println("Exception occured");
+        }
+        
+    }
+}
+
---
+++ handlingError/UseFinally.java
@@ -0,0 +1,40 @@
+package handlingError;
+
+public class UseFinally{
+    public static void genException(int what){
+        int t ;
+        int nums [] = new int[2];
+
+        System.out.println("Receiving " + what);
+        try{
+            switch(what){
+                case 0:
+                    t = 10;
+                    break;
+                case 1:
+                    nums[4] = 4;
+                    break;
+                case 2:
+                    return;
+            }
+        }catch(ArithmeticException exc){
+            System.out.println("can't divide by Zero!");
+            return;
+        }catch(ArrayIndexOutOfBoundsException exc){
+            System.out.println("No matching element found");
+        }
+        finally{
+            System.out.println("Leaving try.");
+        }
+
+    }
+}
+
+class FinallyDemo{
+    public static void main(String [] args){
+        for(int i = 0; i< 3; i++){
+            UseFinally.genException(i);
+            System.out.println();
+        }
+    }
+}
